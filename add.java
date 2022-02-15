6ef30237c9bde376f70ca39dfc3619c21bc01700
Rajneesh Yadav <yadav.rajyadav1@gmail.com>
2022-02-15 16:46:34 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,1 +1,9 @@
-
+public class add
+{  
+public static void main(String args[])   
+{  
+int n1 = 200, n2 = 125, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
